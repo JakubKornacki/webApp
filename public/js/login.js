4107973af424699f0f2ef55bee9e81aacf259291
Jakub Kornacki <jakubkornacki358@gmail.com>
2021-03-22 11:04:35 +0100

    Login and register added

---
+++ public/js/login.js
@@ -0,0 +1,14 @@
+function login(){
+    let email = document.getElementById("exampleInputEmail1").value;
+    let password = document.getElementById("exampleInputPassword1").value;
+    firebase.auth().createUserWithEmailAndPassword(email,password).then((userCredential)=>{
+        var user = userCredential.user;
+        window.location.href = "/secure.html";
+        console.log("user");
+    })
+    .catch((error)=>{
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        console.log("errorMessage,errorCode");
+    });
+}
---
+++ public/js/register.js
@@ -0,0 +1,15 @@
+function register(){
+    let email = document.getElementById("exampleInputEmail1").value;
+    let password = document.getElementById("exampleInputPassword1").value;
+
+    firebase.auth().createUserWithEmailAndPassword(email,password).then((userCredential)=>{
+        var user = userCredential.user;
+        window.location.href = "/secure.html";
+        console.log("user");
+    })
+    .catch((error)=>{
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        console.log("errorMessage,errorCode");
+    })
+}
